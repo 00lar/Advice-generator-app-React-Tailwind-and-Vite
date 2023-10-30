52cc2110879f089ca6b6542cefd41c9210326df7
00lar <eduardani84@gmail.com>
2023-10-29 23:46:19 -0600

    I finished the styles

---
+++ vite-project/src/Components/BtnRandom/index.jsx
@@ -0,0 +1,11 @@
+
+const BtnRandom = () => { 
+    return (
+        <button
+        className=" w-14 h-14 rounded-full bg-Primary2 absolute left-1/2 transform -translate-x-1/2 -bottom-8">
+            
+        </button>
+    )
+}
+
+export {BtnRandom}
--- vite-project/src/Components/Card/index.jsx
+++ vite-project/src/Components/Card/index.jsx
@@ -1,10 +1,19 @@
-
+import { BtnRandom } from "../BtnRandom"
 
 const Card = () => {
     return (
         <div className=" w-screen h-screen flex justify-center items-center">
-        <div className=" w-11/12 h-5/6 bg-Neutral1 " >
-            <h1>Card</h1>
+        <div className=" w-11/12 h-auto max-w-md bg-Neutral1 rounded-2xl " >
+            <div>
+                <div className="w-full flex flex-col justify-center text-center p-4 border-b border-Primary1">
+                    <p className=" text-Primary2 text-sm tracking-[0.3rem] mt-8 mb-8">Advice # 117</p>
+                    <p className=" text-Primary1 text-3xl mb-4">"It is easy to sit up and take notice, whats difficult is getting up an taking action"</p>
+                </div>
+                <div className=" w-full h-24 relative">
+                    <BtnRandom/>
+                </div>
+                
+            </div>
         </div>
         </div>
     )
